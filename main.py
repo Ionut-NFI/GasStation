895689b6a0d94535ec64145a784a84e2bd2ae831
Ionut-NFI <neaguflorinionut@yahoo.com>
2020-10-14 00:33:08 +0300

    resolving partial bugs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
 import threading
 import time
+import os
 
 exitFlag = 0
 
 class Car:
     
-    def __init__(self, carTank):
+    def __init__(self, carTank, name):
         self.tank = carTank
+        self.name = name
 
 class Truck:
     
@@ -18,16 +20,18 @@ class Truck:
 
 def worker(fuelTank,carList):
     
-    selectedCar =random.choice(carList) 
-    
-    if fuelTank >= selectedCar.tank :  
-        fuelTank -= selectedCar.tank
-        print(selectedCar)
+    while(fuelTank > 0):
+        time.sleep(3)
         
-    
+        selectedCar = random.choice(carList)
         
-    return fuelTank
-
+        if fuelTank >= selectedCar.tank :  
+            fuelTank -= selectedCar.tank
+          
+            #print("\n" + "CAR " + str( selectedCar.name) + "\n"+"Fuel tank " + str(fuelTank) )
+            writeLogFile(fuelTank, str( selectedCar.name))
+            
+        
 def my_service():
     if 1000-fuelTank <= truck1.tank:  
         fuelTank+=truck1.tank
@@ -41,23 +45,38 @@ def my_service():
         fuelTank+=truck3.tank
         print("truck 3")
     
+def writeLogFile(fuelTank,car):
+    if(not os.path.isfile("log.txt") ):
+        f = open("log.txt", "w")
+        f.close()
+    else:
+        f = open("log.txt", "a+")
+        toWrite = "\n" + "CAR " + car + "\n"+"Fuel tank " + str(fuelTank)
+        f.write(toWrite)
+        f.close()
 
 if(__name__=="__main__"):
-    global fuelTank 
-    fuelTank = 100 
     
+    fuelTank = 400 
+   
     truck1 = Truck(random.randint(500,800))
     truck2= Truck(random.randint(500,800))
     truck3 = Truck(random.randint(500,800))
     
-    car1 = Car(1)
-    car2 = Car(random.randint(50,80))
-    car3 = Car(random.randint(50,80))
-    car4 = Car(random.randint(50,80))
-    car5 = Car(random.randint(50,80))
+    car1 = Car(1, "1")
+    car2 = Car(random.randint(50,80), "2")
+    car3 = Car(random.randint(50,80), "3")
+    car4 = Car(random.randint(50,80), "4")
+    car5 = Car(random.randint(50,80), "5")
     
+    carList = []
+    carList.append(car1)
+    carList.append(car2)
+    carList.append(car3)
+    carList.append(car4)
+    carList.append(car5)
     
-    q = True
+    print("Fuel capacity for gas station "+ str(fuelTank))
     print("Fuel capacity for car1 "+ str (car1.tank))
     print("Fuel capacity for car2 "+ str (car2.tank))
     print("Fuel capacity for car3 "+ str (car3.tank))
@@ -72,13 +91,15 @@ if(__name__=="__main__"):
     print("--------------------------------")
     time.sleep(3)
     
-    while(q):
+    while(fuelTank > 0 ):
+        
         x = threading.Thread(target = worker, args = (fuelTank,carList,))
-        y = threading.Thread(target = my_service)
+        #y = threading.Thread(target = my_service)
         x.start()
-        #fuelTank = worker(fuelTank)
+        fuelTank = worker(fuelTank, carList)
         print("------ FUEL TANK------ " + str(fuelTank))
-        time.sleep(2)
-        q = False
+        #time.sleep(2)
+        
+    print("FINISH")
         
             
